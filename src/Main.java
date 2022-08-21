4ddd7a3f95aaa2bbcaae88d822aa72ad3c53bfd7
alt1983 <timofeev_a_v@mail.ru>
2022-08-21 23:01:10 +0300

    Commit 2

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,12 @@ public class Main {
     public static void main(String[] args) {
         //Черты функционального стиля:
         // данные не изменяются и ипользуется детерминированная функция без побочных эффектов
-        final String phrase = "тебе уважаемый человек здоровья и побольше";
+        final String phrase = "тебе тебе уважаемый и человек и и здоровья и побольше а";
         Function<String, List<String>> valueConverter = x -> {
             String[] arr1 = x.split(" ");
             List<String> list1 = new ArrayList<>(Arrays.asList(arr1));
             Stream stream1 = list1.stream();
-            List<String> str1 = (List<String>) stream1.sorted(Comparator.naturalOrder()).collect(Collectors.toList());
+            List<String> str1 = (List<String>) stream1.sorted(Comparator.naturalOrder()).distinct().collect(Collectors.toList());
             return str1;
         };
         for (String s : valueConverter.apply(phrase)) {
